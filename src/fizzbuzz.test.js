a050529b8e7db5833926aeffe712f381f186d7c6
alon55 <alonzager5@gmail.com>
2023-02-23 08:49:09 +0100

    green 3

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -3,3 +3,7 @@ const fizzbuzz = require('./fizzbuzz');
 test('returns 1 for the number 1', () => {
     expect(fizzbuzz(1)).toEqual(1);
 });
+
+test('returns "Fizz" for the number 3', () => {
+    expect(fizzbuzz(3)).toEqual('Fizz');
+});
